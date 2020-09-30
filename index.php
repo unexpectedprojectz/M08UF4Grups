be905e4830dea663909b4d168b9017770a512e10
unexpectedprojectz <alexjimenezcamacho76@gmail.com>
2020-09-30 19:55:40 +0200

    AJC - Modificació funcions de l'arxiu.

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
     <body>
         <?php
         function AJC_funcio1() {
-            echo "Prueba 1";
+            echo "Prueba 1 modificació";
         }
         function AJC_funcio2() {
-            echo "Prueba 2";
+            echo "Prueba 2 modificació";
         }
         function AJC_funcio3() {
-            echo "Prueba 3";
+            echo "Prueba 3 modificació";
         }
         ?>
     </body>
