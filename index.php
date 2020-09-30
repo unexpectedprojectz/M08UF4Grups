35d1daadef7c83cfa316290569ebb63889c47349
unexpectedprojectz <alexjimenezcamacho76@gmail.com>
2020-09-30 19:32:57 +0200

    AJC - Creem l'arxiu inicial.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+
+    <body>
+        <?php
+        function AJC_funcio1() {
+            echo "Prueba 1";
+        }
+        function AJC_funcio2() {
+            echo "Prueba 2";
+        }
+        function AJC_funcio3() {
+            echo "Prueba 3";
+        }
+        ?>
+    </body>
+
+</html>
